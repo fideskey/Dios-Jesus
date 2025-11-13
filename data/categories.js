977f3de77bb128290a3d597295233ff74ca57e44
fideskey <fideskey@outlook.com>
2025-11-13 19:04:12 +0100

    Create categories.js

---
+++ data/categories.js
@@ -0,0 +1,10 @@
+export const categories = [
+  { id: 'all', name: 'Todas las Categorías' },
+  { id: 'politics', name: 'Política y Gobierno' },
+  { id: 'economy', name: 'Economía y Finanzas' },
+  { id: 'sports', name: 'FUTBOL' },
+  { id: 'technology', name: 'Tecnología' },
+  { id: 'health', name: 'Salud y Ciencia' },
+  { id: 'culture', name: 'Cultura y Entretenimiento' },
+  { id: 'international', name: 'Internacional' }
+];
